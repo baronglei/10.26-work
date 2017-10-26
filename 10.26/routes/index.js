8223073ae3521adb4c705bd2c8f643dc0a668670
baronglei <962534602@qq.com>
2017-10-26 19:54:13 +0800

    数据库返回到页面变表格

--- 10.26/routes/index.js
+++ 10.26/routes/index.js
@@ -3,29 +3,25 @@ var router = express.Router();
 var fs = require('fs');
 /* GET home page. */
 router.post('/brl', function(req, res, next) {
-	
-	
-	
+
 	res.header('Access-Control-Allow-Origin', '*') //解决跨域问题
 
 	fs.readFile('brl.txt', 'utf-8', function(err, data) {
-		
+
 		var num = JSON.parse(data)
-		
-		num.push({name:'name',content:'content'})
-		
+
+		num.push({
+			name: 'name',
+			content: 'content'
+		})
+
 		console.log(data);
-		fs.writeFile('brl.txt', JSON.stringify(data) , function(err) {
-			res.render('index', {
-				title: 
+		fs.writeFile('brl.txt', JSON.stringify(data), function(err) {
+			res.send({
+				'name': temp
 			});
 		})
 	})
-
-	
-	res.send({
-		'name': temp
-	});
 
 });
 
